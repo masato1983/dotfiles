238f30cd0e2ce0eca026539cc9fb5017abd87e31
Masato Furuta <50264750+masato1983@users.noreply.github.com>
2025-12-20 10:19:06 +0900

    feat(telescope): キーマップと設定を刷新
    Telescope のキーマップを plugins/telescope.lua に集約し、公式 API を利用する形に変更した。
    keymaps.lua から Telescope 関連のマッピングを削除した。
    Telescope のバージョン指定を branch から tag に変更し、v0.2.0 を利用するようにした。
    
    - find_files, live_grep などのファイル検索系を builtin API で再定義
    - git_commits, git_branches など Git 系ピッカーも公式 API で再定義
    - desc オプションで各マッピングの説明を追加
    - harpoon, fzf 拡張機能のロードを明示
    
    これにより設定の一元化と保守性向上を図った。

--- nvim/lua/furuta/plugins/telescope.lua
+++ nvim/lua/furuta/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
+  tag = "v0.2.0",
   dependencies = {
     "nvim-lua/plenary.nvim",
     {
@@ -11,7 +11,9 @@ return {
   },
   config = function()
     local telescope = require("telescope")
+    local builtin = require("telescope.builtin")
     local actions = require("telescope.actions")
+    local keymap = vim.keymap
 
     telescope.setup({
       defaults = {
@@ -27,12 +29,32 @@ return {
     telescope.load_extension("fzf")
     telescope.load_extension("harpoon")
 
-    local keymap = vim.keymap
+    -- File Pickers
+    keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Lists files in your current working directory, respects .gitignore" })
+    keymap.set("n", "<leader>gf", builtin.git_files, { desc = "Fuzzy search through the output of git ls-files command, respects .gitignore" })
+    keymap.set("n", "<leader>gs", builtin.grep_string, { desc = "Searches for the string under your cursor or selection in your current working directory" })
+    keymap.set(
+      "n",
+      "<leader>lg",
+      builtin.live_grep,
+      { desc = "Search for a string in your current working directory and get results live as you type, respects .gitignore." }
+    )
 
-    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-    keymap.set("n", "<leader>fm", "<cmd>Telescope harpoon marks<cr>", { desc = "[F]ind harpoon [M]arks" })
+    -- Git Pickers
+    keymap.set(
+      "n",
+      "<leader>gc",
+      builtin.git_commits,
+      { desc = "Lists git commits with diff preview, checkout action <cr>, reset mixed <C-r>m, reset soft <C-r>s and reset hard <C-r>h" }
+    )
+    keymap.set("n", "<leader>gb", builtin.git_bcommits, { desc = "Lists buffer's git commits with diff preview and checks them out on <cr>" })
+    keymap.set("n", "<leader>gr", builtin.git_bcommits_range, {
+      desc = "Lists buffer's git commits in a range of lines. Use options from and to to specify the range. In visual mode, lists commits for the selected lines",
+    })
+    keymap.set("n", "<leader>gB", builtin.git_branches, {
+      desc = "Lists all branches with log preview, checkout action <cr>, track action <C-t>, rebase action<C-r>, create action <C-a>, switch action <C-s>, delete action <C-d> and merge action <C-y>",
+    })
+    keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Lists current changes per file with diff preview and add action. (Multi-selection still WIP)" })
+    keymap.set("n", "<leader>gS", builtin.git_stash, { desc = "Lists stash items in current repository with ability to apply them on <cr>" })
   end,
 }
