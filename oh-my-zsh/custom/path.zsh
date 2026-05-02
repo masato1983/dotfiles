51b28757ea25f1734dd683812337abc988c8f0ba
Masato Furuta <50264750+masato1983@users.noreply.github.com>
2026-05-02 18:59:41 +0900

    chore: プラグイン更新とPATH設定の改善
    Neovim プラグインのバージョンを複数更新しました。
    最新の機能やバグ修正を取り込み、安定性を向上させています。
    
    .gitconfig に coderabbit の machineId を追加しました。
    これにより coderabbit 関連の設定が有効になります。
    
    .zsh の PATH 設定を改善しました。
    - $HOME/.local/bin を PATH の先頭に追加し、ローカルユーザーのバイナリが優先されるようにしました。
    - 不要な空行を削除し、可読性を向上させました。
    
    これらの変更により、開発環境の利便性と拡張性が向上します。

--- oh-my-zsh/custom/path.zsh
+++ oh-my-zsh/custom/path.zsh
@@ -1,3 +1,6 @@
+# local user binaries
+export PATH="$HOME/.local/bin:$PATH"
+
 # MAMP(PHP,MySQL)
 export PATH="/Applications/MAMP/bin/php/php8.3.14/bin:$PATH"
 export PATH="/Applications/MAMP/Library/bin:$PATH"
@@ -5,4 +8,3 @@ export PATH="/Applications/MAMP/Library/bin/mysql80/bin:$PATH"
 
 # /usr/local/sbin
 export PATH="/usr/local/sbin:$PATH"
-
