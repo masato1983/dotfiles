20ef082c79c7db8dba3ade5cf81059c64f80f12a
Masato Furuta <50264750+masato1983@users.noreply.github.com>
2026-05-02 19:13:01 +0900

    refactor(alias): 使わない alias を大幅整理
    不要な alias を多数削除しました。
    .zshrc の可読性と保守性を向上させるための対応です。
    
    主な変更点:
    - 使われていない cd 系 alias の削除
    - 編集・PHP・fd・git・GitHub CLI 関連 alias の削除
    - shellcheck, php-cs-fixer, phpstan, ポート確認などの alias を削除
    - コメントや空行も整理
    
    今後は必要な alias のみを厳選して管理します。

--- oh-my-zsh/custom/alias.zsh
+++ oh-my-zsh/custom/alias.zsh
@@ -1,5 +1,4 @@
 # general
-alias c='code .'
 alias cl='clear'
 alias e='exit'
 alias s='source ~/.zshrc'
@@ -7,23 +6,16 @@ alias s='source ~/.zshrc'
 # cd
 alias home='cd ~/'
 alias download='cd ~/Download'
-alias omz='cd ~/.oh-my-zsh'
 alias dotfiles='cd ~/.dotfiles'
 alias kapture='cd ~/Movies/Kaptures'
 alias trash='cd ~/.local/share/Trash'
 alias icloud='cd ~/Library/Mobile\ Documents/com~apple~CloudDocs'
-alias appsupport='cd ~/Library/Application\ Support'
-alias mamp='cd /Applications/MAMP'
 
 # echo
 alias exitcode='echo $?'
 
 # pwd
 alias pwdcp='pwd | tee >(pbcopy)'
-
-# edit
-alias zshrc='nvim ~/.zshrc'
-alias zprofile='nvim ~/.zprofile'
 
 # global
 alias -g B='| bat'
@@ -63,17 +55,8 @@ alias ipp='curl ipinfo.io'
 alias ipen0='ipconfig getifaddr en0'
 alias ipen1='ipconfig getifaddr en1'
 
-# php server
-alias psl='php -S localhost:8000 -t'
-alias pse='php -S 192.168.0.252:8000 -t'
-
 # xserver
 alias xserver-upload='rsync -avhe "ssh -p 10022" --delete --update --exclude-from=".rsyncignore" ./ masa1122@sv7051.xserver.jp:/home/masa1122/coding11ty.com/public_html/test.coding11ty.com/'
-
-# oh my zsh
-alias opg='cd /Users/masato/.oh-my-zsh/plugins/git/'
-alias opn='cd /Users/masato/.oh-my-zsh/plugins/npm/'
-alias opn='cd /Users/masato/.oh-my-zsh/plugins/rsync/'
 
 # homebrew
 alias brewup='brew update && brew upgrade && brew cleanup && brew doctor'
@@ -95,18 +78,6 @@ alias tls='tmux list-sessions || tmux ls'
 alias tns='tmux new-session -s'
 alias tks='tmux kill-session -t'
 
-# fd
-alias fdf='fd --type file'
-alias fdd='fd --type directory'
-alias fd0='fd --hidden --changed-within $(date -v-0d '+%Y-%m-%d') --type file -X eza --all --long --git --octal-permissions --icons=auto --color-scale=all --sort=modified --time-style="+%m-%d %H:%M"'
-alias fd1='fd --hidden --changed-within $(date -v-1d '+%Y-%m-%d') --type file -X eza --all --long --git --octal-permissions --icons=auto --color-scale=all --sort=modified --time-style="+%m-%d %H:%M"'
-alias fd2='fd --hidden --changed-within $(date -v-2d '+%Y-%m-%d') --type file -X eza --all --long --git --octal-permissions --icons=auto --color-scale=all --sort=modified --time-style="+%m-%d %H:%M"'
-alias fd3='fd --hidden --changed-within $(date -v-3d '+%Y-%m-%d') --type file -X eza --all --long --git --octal-permissions --icons=auto --color-scale=all --sort=modified --time-style="+%m-%d %H:%M"'
-alias fd4='fd --hidden --changed-within $(date -v-4d '+%Y-%m-%d') --type file -X eza --all --long --git --octal-permissions --icons=auto --color-scale=all --sort=modified --time-style="+%m-%d %H:%M"'
-alias fd5='fd --hidden --changed-within $(date -v-5d '+%Y-%m-%d') --type file -X eza --all --long --git --octal-permissions --icons=auto --color-scale=all --sort=modified --time-style="+%m-%d %H:%M"'
-alias fd6='fd --hidden --changed-within $(date -v-6d '+%Y-%m-%d') --type file -X eza --all --long --git --octal-permissions --icons=auto --color-scale=all --sort=modified --time-style="+%m-%d %H:%M"'
-alias fd7='fd --hidden --changed-within $(date -v-7d '+%Y-%m-%d') --type file -X eza --all --long --git --octal-permissions --icons=auto --color-scale=all --sort=modified --time-style="+%m-%d %H:%M"'
-
 # ripgrep
 alias rgh='rg --hidden --glob "!.git" --glob "!package-lock.json"'
 alias rgl='rg --files-with-matches'
@@ -116,29 +87,8 @@ alias rghl='rg --hidden --files-with-matches --glob "!.git" --glob "!package-loc
 alias tree-all='tree -a -I .git'
 alias tree-all-links='tree -a -l -I .git'
 
-# git
-alias grpa='git restore --patch'
-alias grpas='git restore --patch --staged'
-alias gpod='git pull origin develop'
-alias gdu='git diff $(git_current_branch)..origin/$(git_current_branch)'
-alias gdun='git diff $(git_current_branch)..origin/$(git_current_branch) --name-status'
-alias gloggp='git log --graph --pretty="%Cred%h%Creset %s -%C(auto)%d%Creset %Cgreen(%ar) %C(bold blue)<%an>%Creset"'
-alias gloggpa='git log --graph --pretty="%Cred%h%Creset %s -%C(auto)%d%Creset %Cgreen(%ar) %C(bold blue)<%an>%Creset" --all'
-alias gloggps='git log --graph --pretty="%Cred%h%Creset %s -%C(auto)%d%Creset %Cgreen(%ar) %C(bold blue)<%an>%Creset" --stat'
-alias gcpn='git cherry-pick --no-commit'
-alias gpot='git push origin --tags'
-
 # github cli
 alias ghb='gh browse'
-alias ghil='gh issue list'
-alias ghol='gh org list'
-alias ghprc='gh pr create'
-alias ghprm='gh pr merge'
-alias ghprl='gh pr list'
-alias ghprlc='gh pr list --closed'
-alias ghrv='gh repo view'
-alias ghrl='gh run list'
-alias ghrw='gh run watch'
 
 # fork
 alias f='fork'
@@ -154,23 +104,11 @@ alias lt='eza --all --long --git --octal-permissions --no-user --icons=auto --co
 alias tolower="pbpaste | tr '[:upper:]' '[:lower:]' | pbcopy"
 alias toupper="pbpaste | tr '[:lower:]' '[:upper:]' | pbcopy"
 
-# php cs fixer
-alias csfixer='php tools/php-cs-fixer/vendor/bin/php-cs-fixer'
-
-# php stan
-alias stan='vendor/bin/phpstan analyse'
-
-# shellcheck
-alias sc='shellcheck --color=always'
-
 # identify
 alias dis="identify -format 'サイズ: %b ピクセル寸法: %wx%h\n'"
 
 # history
 alias history='fc -li 1'
-
-# other
-alias portcheck8888='sudo lsof -P -i:8888'
 
 # gsed (GNU sed)
 alias sed-empty='gsed -i "/^$/d"'                               # 空行を削除
